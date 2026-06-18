7f6ab0c0d7814a7e6fcc01fcd999c20470052ca6
HuyDangXuan <hhunu114@gmail.com>
2026-06-18 10:29:33 +0700

    Enhance media retrieval to support file downloads

--- controllers/media.controller.ts
+++ controllers/media.controller.ts
@@ -4,8 +4,13 @@ import fs from 'fs';
 
 export const getMedia = (req: Request, res: Response) => {
   const filename = req.params.filename;
+  const type = req.query.type;
 
   const mediaDir = path.join(__dirname, '../media', `${filename}`);
 
-  res.sendFile(mediaDir);
+  if (type === 'download') {
+    res.download(mediaDir);
+  } else {
+    res.sendFile(mediaDir);
+  }
 }
